e211499059ac4db9f9751d5c5c915840465633cc
Tan <tan10tvo@gmail.com>
2021-12-11 14:01:05 -0700

    commit tree 99e05615f1681e75f1a67a2d73dab559faf8286c

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+
+  
+
+
+
+    return(
+        <button 
+        onFocus={(e) => {
+            console.log('Good!');
+           }}
+        onBlur={(e) => {
+            console.log("Hey! Eyes on me!");
+          }}
+        
+         >
+             
+            Eyes on me
+         
+         
+         </button>
+    );
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    
+    function handleChange(event) {
+        console.log("Entering password...");
+    }
+
+    return(
+        <div>
+            <input 
+            type="password"
+            onChange={handleChange} 
+             />
+        </div>
+    );
+}
+
+export default Keypad
